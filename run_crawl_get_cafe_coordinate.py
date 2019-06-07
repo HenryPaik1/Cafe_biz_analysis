834068961e16cf104cb92ecd6046037a90e15c5c
HenryPaik1 <renwenxue0@gmail.com>
2019-06-08 01:12:38 +0900

    edit sys.argv

--- run_crawl_get_cafe_coordinate.py
+++ run_crawl_get_cafe_coordinate.py
@@ -6,13 +6,16 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     print('start crawling')
-    
+
     #with open('/home/ubuntu/Cafe_biz_analysis/resource/cafe_all_addr.pickle', 'rb') as f:
     with open(package_dir + '/resource/cafe_all_addr.pickle', 'rb') as f:
         addr_ls = pickle.load(f)
-    
+    # num: 1, ..., 7
     num = int(sys.argv[1])
-    crawl_size = int(10000 * num)
+    from_, to_ = 10000*(num-1), 10000*num
+    try:
+        addr_ls = addr_ls[from_:to_]
+    except:
+        addr_ls = addr_ls[to_:]
 
-    addr_ls = addr_ls[:crawl_size]
     get_coordinates(addr_ls, num)
