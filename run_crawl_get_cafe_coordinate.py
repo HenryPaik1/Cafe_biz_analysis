414230b0755731e69847bdb41167826e9446dad0
HenryPaik1 <renwenxue0@gmail.com>
2019-06-08 18:00:48 +0900

    adjust crawl size

--- run_crawl_get_cafe_coordinate.py
+++ run_crawl_get_cafe_coordinate.py
@@ -7,12 +7,12 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     print('start crawling')
 
-    #with open('/home/ubuntu/Cafe_biz_analysis/resource/cafe_all_addr.pickle', 'rb') as f:
-    with open(package_dir + '/resource/cafe_all_addr.pickle', 'rb') as f:
+    #with open('/home/ubuntu/Cafe_biz_analysis/resource/unique_cafe_addr.pickle', 'rb') as f:
+    with open(package_dir + '/resource/unique_cafe_addr.pickle', 'rb') as f:
         addr_ls = pickle.load(f)
     # num: 1, ..., 7
     num = int(sys.argv[1])
-    from_, to_ = 10000*(num-1), 10000*num
+    from_, to_ = 3000*(num-1), 3000*num
     try:
         addr_ls = addr_ls[from_:to_]
     except:
